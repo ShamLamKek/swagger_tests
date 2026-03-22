e84a381a3f45820a4ea99ebb8c4660cc66a0de25
ShamLamKek <csgo.sham@yandex.ru>
2026-03-22 12:41:55 +0800

    add negative create test

--- suites/functional/pet/test_create_pet.py
+++ suites/functional/pet/test_create_pet.py
@@ -22,3 +22,20 @@ def test_create_pet_positive(pet_api):
         pet = PetResponse(**response_data)  # валидация через Pydantic
         assert pet.name == "doggie"
         assert pet.id is not None
+
+
+@allure.feature("Pet")  # группа функциональности
+@allure.story("Create pet")  # конкретная история
+@allure.title("Негативный тест создания питомца")  # название теста
+def test_create_pet_negative(pet_api):
+    with allure.step("Подготовить тело запроса"):
+        body = {
+            "name": 123,
+            "photoUrls": 123
+        }
+
+    with allure.step("Отправить POST-запрос"):
+        response = pet_api.post(body)
+
+    with allure.step("Проверить ответ"):
+        assert response.status_code == 405
